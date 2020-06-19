a81b05215a6767ae55968a8b696e654e704174a3
Ilya Pro <ilya.pro.dev@gmail.com>
2020-06-19 21:03:44 +0300

    заготовка страниц

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    {{ health.alive }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
 
+  },
+  data() {
+    return {
+      health: {}
+    }
+  },
+  mounted() {
+    this.getHealth();
+  },
+  methods: {
+    getHealth() {
+      const filter = '';
+      axios
+        .get('https://rebus-leadersofdigital-2020.herokuapp.com/api/health/' + filter)
+        .then(response => {
+          console.log('roger that', response.data);
+          this.health = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 }
 </script>
